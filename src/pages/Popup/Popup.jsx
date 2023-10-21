fc7ea5fe873fcaa6c57278f93be6ca44c627e956
AJ <yspdev@gmail.com>
2023-10-21 12:26:36 +1100

    add text field and submit button

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from '../../assets/img/logo.png';
-import Greetings from '../../containers/Greetings/Greetings';
-import './Popup.css';
+import React from "react";
+import logo from "../../assets/img/logo.png";
+import Greetings from "../../containers/Greetings/Greetings";
+import "./Popup.css";
 
 const Popup = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HELLO! I AM CLICKOLAS CAGE!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
+        <p>HELLO! I AM CLICKOLAS CAGE!</p>
+        <input type="text" placeholder="Add event x to my google calendar" />
+        <input type="button" Value="Submit"/>
       </header>
     </div>
   );
