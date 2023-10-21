1e7904a6ff173fb5c23c3d491913cbb3264080fb
AJ <yspdev@gmail.com>
2023-10-21 14:08:24 +1100

    working openai api

--- clickolas-cage/src/popup/Popup.jsx
+++ clickolas-cage/src/popup/Popup.jsx
@@ -1,51 +1,51 @@
-import React, { useRef } from "react";
-import logo from "../assets/logo.png";
-import "./Popup.css";
-import OpenAI from "openai";
+import React, { useRef, useState } from 'react'
+import logo from '../assets/logo.png'
+import './Popup.css'
+import OpenAI from 'openai'
 
-// const openaiInstance = new OpenAI({
-//   apiKey: "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", // defaults to process.env["OPENAI_API_KEY"]
-// });
+const openai = new OpenAI({
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY, // defaults to process.env[""]
+  dangerouslyAllowBrowser: true,
+})
 
-
+console.log();
 const sendPromptToOpenAI = async (prompt) => {
-  const chatCompletion = await openaiInstance.chat.completions.create({
-    model: "gpt-4",
+  console.log('thinking...')
+  const chatCompletion = await openai.chat.completions.create({
+    model: 'gpt-4',
     messages: [
       {
-        role: "user",
+        role: 'user',
         content: `I want to provide step by step single commands to be inputted into a chrome console to achieve the following task : ${prompt}.
         First only provide the command to which URL to navigate to. then I will provide the DOM elements you can interact with, afterwards provide a command to interact with the dom and we will continue this process until the task is achieved`,
       },
     ],
-    stream: true,
-  });
-  console.log(chatCompletion);
-  // return response.choices[0].text.strip();
-};
+  })
+  console.log(chatCompletion.choices[0].message.content)
+  return chatCompletion.choices[0].message.content
+  // return chatCompletion.choices[0].text.strip()
+}
 
 const Popup = () => {
-  const promptRef = useRef(null);
+  const promptRef = useRef(null)
+  const [LLMThoughts, setLLMThoughts] = useState(null)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>HELLO! I AM CLICKOLAS CAGE!</p>
+        <input ref={promptRef} type="text" placeholder="Add event x to my google calendar" />
         <input
-          ref={promptRef}
-          type="text"
-          placeholder="Add event x to my google calendar"
-        />
-        <input
-          onClick={() => {
-            sendPromptToOpenAI(e.target.value);
+          onClick={async () => {
+            setLLMThoughts(await sendPromptToOpenAI(promptRef.current))
           }}
           type="button"
           value="Submit"
         />
       </header>
+      <p>{LLMThoughts}</p>
     </div>
-  );
-};
+  )
+}
 
-export default Popup;
+export default Popup
