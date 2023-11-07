62026430c2eef212a1ad284243ef9a1a6878e1da
Arash Joobandi <yspdev@gmail.com>
2023-11-07 18:59:58 +1100

    json and gpt-4 turbo

--- clickolas-cage/src/helpers.js
+++ clickolas-cage/src/helpers.js
@@ -8,7 +8,7 @@ const openai = new OpenAI({
 export const getNextAction = (plan, currentStep) => {
   console.log(plan, currentStep)
   try {
-    if(currentStep > plan.split('\n').length) return null;
+    if (currentStep > plan.split('\n').length) return null
     let currentTask = plan.split('\n')[currentStep]
     const actionParts = currentTask.split('(')
     const actionParams = actionParts[1]?.slice(0, -1).split(',')
@@ -18,7 +18,7 @@ export const getNextAction = (plan, currentStep) => {
     if (matches) return { actionName: matches[0], actionParams, currentTask, currentStep }
     else getNextAction(plan, currentStep + 1)
   } catch (e) {
-    getNextAction(plan, currentStep + 1);
+    getNextAction(plan, currentStep + 1)
   }
 }
 
@@ -37,32 +37,30 @@ export const sendMessageToContentScript = async (prompt, tabId = null) => {
 }
 
 export const sendPromptToPlanner = async (prompt) => {
+  // make an event in my google calendar on friday 12pm labeled "hello world"
   console.log('thinking about it...')
   const chatCompletion = await openai.chat.completions.create({
-    model: 'gpt-4',
+    model: 'gpt-4-1106-preview',
+    response_format: { type: 'json_object' },
     messages: [
       {
         role: 'system',
-        content: `you are an expert web browsing AI with the following functions available:
-        1. Navigate to a URL (Codename: NAVURL({url}))
-        2. Click a button (Codename: CLICKBTN({buttonId}))
-        3. Input text (Codename: INPUT({fieldId},{textToInput}))
-        4. Select an option in a form (Codename: SELECT({fieldId},{option}))
-        5. Wait for page load and inspect contents of page (Codename: WAITLOAD())
-        6. Ask the user for more information (Codename: ASKUSER({question})
-        given a prompt from the user provide a step by step plan to execute it in a web browser only utilizing the 6 functions above using the relative codenames.
+        content: `you are an expert web browsing AI given a prompt from the user provide a step by step plan to execute it in a web browser.
         if you are unsure of URL, Ask the user. if you are unsure of IDs wait for page load.
-        ALWAYS start with NAVURL.`,
+        ALWAYS start with NAVURL. Provide response with this JSON schema:
+        {
+          plan: [ {
+            action: "NAVURL" | "CLICKBTN" | "INPUT" | "SELECT" | "WAITLOAD" | "ASKUSER",
+            param: "url" | "divId" | "question"
+            param2?: "optionToSelect" | "textToInput"
+          },...]
+        }
+        `,
       },
       {
         role: 'user',
         content: `your first task is: ${prompt}`,
       },
-      // {
-      //   role: 'user',
-      //   content: `I want to provide step by step single commands to be inputted into a chrome console to achieve the following task : ${prompt}.
-      //   First only provide the command to which URL to navigate to. then I will provide the DOM elements you can interact with, afterwards provide a command to interact with the dom and we will continue this process until the task is achieved`,
-      // },
     ],
   })
   return chatCompletion.choices[0].message.content
