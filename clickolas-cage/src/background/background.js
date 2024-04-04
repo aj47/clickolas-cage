ada360e3ddab37cbbb3269b1ddcd875e4d3e8571
Arash Joobandi <yspdev@gmail.com>
2024-04-04 21:52:30 +1100

    only allow content script on approved tabs

--- clickolas-cage/src/background/background.js
+++ clickolas-cage/src/background/background.js
@@ -13,6 +13,7 @@ let currentStep = 0
 let originalPrompt = ''
 let currentURL = ''
 let recipes = null
+let allowedTabs = new Set();
 
 fetch(chrome.runtime.getURL('src/recipes.json'))
   .then((response) => response.json()) // parse the JSON from the response
@@ -27,6 +28,7 @@ const navURL = (url) => {
   console.log(url, 'url')
   currentURL = url
   chrome.tabs.create({ url: url }, async function (tab) {
+    allowedTabs.add(tab.id);
     targetTab = tab.id // Store the tab ID for later use
     currentStep++
     // Check if the tab is completely loaded before sending a message
@@ -287,3 +289,17 @@ async function clickElement(tabId, selector) {
     console.log(e, 'e')
   }
 }
+
+
+// --- We only allow content script to execute on tabs created by background script
+// Listen for when a tab is closed and remove it from the set
+chrome.tabs.onRemoved.addListener(function(tabId) {
+    allowedTabs.delete(tabId);
+});
+//Used for ContentScript to see if its on a tab it can execute on
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "checkTab") {
+        const isAllowed = allowedTabs.has(sender.tab.id);
+        sendResponse({isAllowed: isAllowed});
+    }
+});
--- clickolas-cage/src/contentScript/contentScript.jsx
+++ clickolas-cage/src/contentScript/contentScript.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import SidePanel from "../sidepanel/SidePanel"
+import SidePanel from '../sidepanel/SidePanel'
 
-console.log("yo");
-const root = document.createElement('div')
-root.id = 'crx-root'
-document.body.append(root)
+// --- We only allow content script to execute on tabs created by background script
+chrome.runtime.sendMessage({ action: 'checkTab' }, function (response) {
+  if (response.isAllowed) {
+    console.log('content script executed')
+    const root = document.createElement('div')
+    root.id = 'crx-root'
+    document.body.append(root)
 
-ReactDOM.render(
-  <React.StrictMode>
-    <SidePanel/>
-  </React.StrictMode>,
-  root,
-)
+    ReactDOM.render(
+      <React.StrictMode>
+        <SidePanel />
+      </React.StrictMode>,
+      root,
+    )
+  }
+})
