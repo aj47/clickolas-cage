600e83b88d8abaf71ab88b6b71edf00370700e67
AJ <yspdev@gmail.com>
2024-08-05 19:50:47 +0700

    transparent clickolas

--- clickolas-cage/src/sidepanel/SidePanel.jsx
+++ clickolas-cage/src/sidepanel/SidePanel.jsx
@@ -25,7 +25,7 @@ export const SidePanel = () => {
   async function showClickIndicator(x, y) {
     const clickIndicator = document.createElement('img')
     clickIndicator.src = chrome.runtime.getURL('img/logo-48.png')
-    clickIndicator.className = 'clickolas-click-indicator'
+    clickIndicator.style.opacity = 0.9;
     clickIndicator.style.position = 'absolute'
     clickIndicator.style.left = `${x - 17}px`  // Center the 34px image
     clickIndicator.style.top = `${y - 17}px`   // Center the 34px image
@@ -265,7 +265,6 @@ export const SidePanel = () => {
   }
 
   useEffect(() => {
-    showClickIndicator(500, 500);
     if (!socketRef.current) {
       socketRef.current = chrome.runtime.onMessage.addListener(
         async function (request, sender, sendResponse) {
