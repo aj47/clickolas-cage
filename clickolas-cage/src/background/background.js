b81480c8d01e8e1720ef2a2075a514e22243c92d
Arash Joobandi <yspdev@gmail.com>
2024-08-19 11:53:15 +1000

    sleep 1s after each step

--- clickolas-cage/src/background/background.js
+++ clickolas-cage/src/background/background.js
@@ -60,8 +60,8 @@ const completedTask = async () => {
   const currentState = getState()
   console.log('Completed task. Step:', currentState.currentStep)
   updateState({ currentStep: currentState.currentStep + 1 })
-  // console.log('sleeping 3s...')
-  // await sleep(3000)
+  console.log('sleeping 1s...')
+  await sleep(1000)
   const updatedState = getState()
   console.log('Moving to next step:', updatedState.currentStep)
   if (updatedState.currentStep >= updatedState.currentPlan.length) {
